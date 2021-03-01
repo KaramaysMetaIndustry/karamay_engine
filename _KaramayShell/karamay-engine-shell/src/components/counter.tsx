18873027e698a72d7174533b239e001b43b5a7ec
pb-jcc <2605257928@qq.com>
2021-03-01 10:26:43 +0800

    update

--- _KaramayShell/karamay-engine-shell/src/components/counter.tsx
+++ _KaramayShell/karamay-engine-shell/src/components/counter.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import {Container, Row, Alert, Button, Col} from 'react-bootstrap'
 
 const counter: React.FC = () =>{
     return(
         <div>
+            <Container fluid>
+                <Row>
+                    <Col>1of1</Col>
+                </Row>
+            </Container>
+
             <h2>0</h2>
             <button>+</button>
+            <Button variant="success">++</Button>
+            <Alert variant="primary">pick me up</Alert>
+            
         </div>
     )
 }
