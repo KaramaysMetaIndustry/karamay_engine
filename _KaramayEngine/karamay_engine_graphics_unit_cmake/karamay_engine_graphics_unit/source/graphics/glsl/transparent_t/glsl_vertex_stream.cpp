8ce548fa250570b1242388b62b1e93e4c07e8867
51WORLD-JCC <2605257928@qq.com>
2021-10-14 18:57:46 +0800

    update

--- _KaramayEngine/karamay_engine_graphics_unit_cmake/karamay_engine_graphics_unit/source/graphics/glsl/transparent_t/glsl_vertex_stream.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by jichengcheng on 2021/10/14.
-//
-
-#include "glsl_vertex_stream.h"
--- _KaramayEngine/karamay_engine_graphics_unit_cmake/karamay_engine_graphics_unit/source/graphics/glsl/transparent_t/glsl_vertex_stream.h
+++ _KaramayEngine/karamay_engine_graphics_unit_cmake/karamay_engine_graphics_unit/source/graphics/glsl/transparent_t/glsl_vertex_stream.h
@@ -1,6 +1,11 @@
 #ifndef GLSL_VERTEX_STREAM_H
 #define GLSL_VERTEX_STREAM_H
 
+#include "graphics/glsl/glsl_class.h"
+
+struct glsl_vertex_stream_descriptor
+{
+};
 
 class glsl_vertex_stream {
 public:
