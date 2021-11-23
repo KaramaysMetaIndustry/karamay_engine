8bd54bb10e766dd5ada87c812967b3a97eb52026
pb-jcc <2605257928@qq.com>
2021-11-23 08:21:27 +0800

    update

--- _KaramayEngine/karamay_engine_graphics_unit_cmake/karamay_engine_graphics_unit/source/graphics/resource/framebuffer/gl_framebuffer.h
+++ _KaramayEngine/karamay_engine_graphics_unit_cmake/karamay_engine_graphics_unit/source/graphics/resource/framebuffer/gl_framebuffer.h
@@ -14,9 +14,56 @@ public:
     virtual void unbind() = 0;
 };
 
+enum class gl_framebuffer_draw_format : GLenum
+{
+    COLOR_INDEX = GL_COLOR_INDEX,
+    STENCIL_INDEX = GL_STENCIL_INDEX,
+    DEPTH_COMPONENT = GL_DEPTH_COMPONENT,
+    
+    RGB = GL_RGB,
+    BGR = GL_BGR,
+    RGBA = GL_RGBA,
+    BGRA = GL_BGRA,
+    RED = GL_RED,
+    GREEN = GL_GREEN,
+    BLUE = GL_BLUE,
+    ALPHA = GL_ALPHA,
+
+    LUMINANCE = GL_LUMINANCE,
+    LUMINANCE_ALPHA = GL_LUMINANCE_ALPHA
+};
+
+enum class gl_framebuffer_draw_type : GLenum
+{
+    BITMAP = GL_BITMAP,
+
+    BYTE = GL_BYTE,
+    SHORT = GL_SHORT,
+    INT = GL_INT,
+    FLOAT = GL_FLOAT,
+
+    UNSIGNED_BYTE = GL_UNSIGNED_BYTE,
+    UNSIGNED_BYTE_3_3_2 = GL_UNSIGNED_BYTE_3_3_2,
+    UNSIGNED_BYTE_2_3_3_REV = GL_UNSIGNED_BYTE_2_3_3_REV,
+    UNSIGNED_SHORT = GL_UNSIGNED_SHORT,
+    UNSIGNED_SHORT_5_6_5 = GL_UNSIGNED_SHORT_5_6_5,
+    UNSIGNED_SHORT_5_6_5_REV = GL_UNSIGNED_SHORT_5_6_5_REV,
+    UNSIGNED_SHORT_4_4_4_4 = GL_UNSIGNED_SHORT_4_4_4_4,
+    UNSIGNED_SHORT_4_4_4_4_REV = GL_UNSIGNED_SHORT_4_4_4_4_REV,
+    UNSIGNED_SHORT_5_5_5_1 = GL_UNSIGNED_SHORT_5_5_5_1,
+    UNSIGNED_SHORT_1_5_5_5_REV = GL_UNSIGNED_SHORT_1_5_5_5_REV,
+    UNSIGNED_INT = GL_UNSIGNED_INT,
+    UNSIGNED_INT_8_8_8_8 = GL_UNSIGNED_INT_8_8_8_8,
+    UNSIGNED_INT_8_8_8_8_REV = GL_UNSIGNED_INT_8_8_8_8_REV,
+    UNSIGNED_INT_10_10_10_2 = GL_UNSIGNED_INT_10_10_10_2,
+    UNSIGNED_INT_2_10_10_10_REV = GL_UNSIGNED_INT_2_10_10_10_REV
+    
+};
+
+
 class gl_framebuffer final : public gl_framebuffer_base{
 public:
-    gl_framebuffer()
+    gl_framebuffer(uint32 width, uint32 height)
     {
         glCreateFramebuffers(1, &_handle);
     }
@@ -28,6 +75,20 @@ public:
     {
         glDeleteFramebuffers(1, &_handle);
     }
+
+public:
+
+    void reallocate(uint32 width, uint32 height)
+    {
+
+    }
+
+
+private:
+
+    std::vector<std::unique_ptr<gl_texture_2d_t>> colors_attachments;
+    std::unique_ptr<gl_texture_2d_t> depth_component;
+    std::unique_ptr<gl_texture_2d_t> stencil_component;
 
 public:
 
@@ -48,6 +109,10 @@ public:
         if (!renderbuffer || attachment_index >= GL_MAX_COLOR_ATTACHMENTS) return;
         glNamedFramebufferRenderbuffer(_handle, GL_COLOR_ATTACHMENT0 + attachment_index, GL_RENDERBUFFER, renderbuffer->get_handle());
     }
+    void set_color_attachment(uint32 attachment_index, const std::shared_ptr<gl_texture_2d_t> texture_2d)
+    {
+        glNamedFramebufferTexture(_handle, GL_COLOR_ATTACHMENT0 + attachment_index, texture_2d->get_handle(), 0);
+    }
     void set_depth_attchment(const std::shared_ptr<gl_renderbuffer>& renderbuffer)
     {
         if (!renderbuffer) return;
@@ -60,15 +125,10 @@ public:
     }
     void set_depth_stencil(const gl_renderbuffer* renderbuffer)
     {
-        if (renderbuffer)
-        {
-            glNamedFramebufferRenderbuffer(_handle, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, renderbuffer->get_handle());
-        }
+        glNamedFramebufferRenderbuffer(_handle, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, renderbuffer->get_handle());
     }
 
-    void set_color_attachment()
-    {
-    }
+   
 
 public:
 
@@ -87,12 +147,41 @@ public:
         glReadBuffer(GL_COLOR_ATTACHMENT0 + color_attachment_index);
     }
 
-public:
-
-    void read_pixels(int32 x, int32 y, int32 width, int32 height)
+    void draw_pixels(int32 width, int32 height, gl_framebuffer_draw_format format, gl_framebuffer_draw_type type, const void* pixels)
     {
-        
-        glReadPixels(x, y, width, height, 0, 0, nullptr);
+        //glPixelStoref();
+        //glPixelTransferf();
+        //glPixelMapfv();
+        //glPixelZoom()
+        glDrawPixels(width, height, static_cast<GLenum>(format), static_cast<GLenum>(type), pixels);
+    }
+
+
+    void draw_depth_stencil(int32 width, int32 height, GLenum type, const void* pixels)
+    {
+        glDrawPixels(width, height, GL_DEPTH_STENCIL, type, pixels);
+    }
+
+    void read_color(int32 attachment_index, int32 x, int32 y, int32 width, int32 height, GLenum format, GLenum type, void* pixels)
+    {
+        glReadBuffer(GL_COLOR_ATTACHMENT0 + attachment_index);
+        glClampColor(GL_CLAMP_READ_COLOR, GL_TRUE); // GL_FALSE, GL_FIXED_ONLY
+        glReadPixels(x, y, width, height, format, type, pixels);
+    }
+
+    void read_depth(int32 x, int32 y, int32 width, int32 height, GLenum type, void* pixels)
+    {
+        glReadPixels(x, y, width, height, GL_DEPTH_COMPONENT, type, pixels);
+    }
+
+    void read_stencil(int32 x, int32 y, int32 width, int32 height, GLenum type, void* pixels)
+    {
+        glReadPixels(x, y, width, height, GL_STENCIL_INDEX, type, pixels);
+    }
+
+    void read_depth_stencil(int32 x, int32 y, int32 width, int32 height, GLenum type, void* pixels)
+    {
+        glReadPixels(x, y, width, height, GL_DEPTH_STENCIL, type, pixels);
     }
 
     void clamp_color()
@@ -113,12 +202,12 @@ public:
         //glCopyTextureSubImage2D()
     }
 
+ private:
+
     void check_status()
     {
         glCheckFramebufferStatus(GL_FRAMEBUFFER);
     }
-
-private:
 
     bool is_framebuffer() const
     {
