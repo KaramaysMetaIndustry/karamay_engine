9712fa547093ad4d5fe9f4b36f6ada6b2787bdc2
pb-jcc <2605257928@qq.com>
2022-01-29 20:07:45 +0800

    delete dirty code

--- _KaramayEngine/karamay_engine_graphics_unit_cmake/karamay_engine_graphics_unit/source/karamay-engine-graphics.cpp
+++ _KaramayEngine/karamay_engine_graphics_unit_cmake/karamay_engine_graphics_unit/source/karamay-engine-graphics.cpp
@@ -7,208 +7,6 @@
 #include "renderers/gl_static_mesh_renderer.h"
 #include "public/json.h"
 
-float vertices[] = {
-		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f, //0
-		 0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  0.0f,
-		 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  1.0f,
-		 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  1.0f,
-		-0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f,
-
-		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  0.0f,
-		 0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  0.0f,
-		 0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  1.0f,
-		 0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  1.0f,
-		-0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  1.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  0.0f,
-
-		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
-		-0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  1.0f,
-		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
-		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
-		-0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  0.0f,
-		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
-
-		 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
-		 0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  1.0f,
-		 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
-		 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
-		 0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  0.0f,
-		 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
-
-		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  1.0f,
-		 0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  1.0f,
-		 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  0.0f,
-		 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  0.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  0.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  1.0f,
-
-		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f,
-		 0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  1.0f,
-		 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  0.0f,
-		 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  0.0f,
-		-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  0.0f,
-		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f //35
-};
-
-std::uint32_t indices[] = {
-	1, 2, 2, 3, 4, 5, 6
-};
-
-//std::vector<glv::f32vec3> offset_positions{
-//	glv::f32vec3(0.0f,  0.0f,  0.0f),
-//	glv::f32vec3(2.0f,  5.0f, -15.0f),
-//	glv::f32vec3(-1.5f, -2.2f, -2.5f),
-//	glv::f32vec3(-3.8f, -2.0f, -12.3f),
-//	glv::f32vec3(2.4f, -0.4f, -3.5f),
-//	glv::f32vec3(-1.7f,  3.0f, -7.5f),
-//	glv::f32vec3(1.3f, -2.0f, -2.5f),
-//	glv::f32vec3(1.5f,  2.0f, -2.5f),
-//	glv::f32vec3(1.5f,  0.2f, -1.5f),
-//	glv::f32vec3(-1.3f,  1.0f, -1.5f)
-//};
-//
-//std::vector<glv::f32vec3> normals{
-//		glv::f32vec3(0.0f, 0.0f, -1.0f),
-//		glv::f32vec3(0.0f, 0.0f, -1.0f),
-//		glv::f32vec3(0.0f, 0.0f, -1.0f),
-//		glv::f32vec3(0.0f, 0.0f, -1.0f),
-//		glv::f32vec3(0.0f, 0.0f, -1.0f),
-//		glv::f32vec3(0.0f, 0.0f, -1.0f),
-//
-//		glv::f32vec3(0.0f, 0.0f, 1.0f),
-//		glv::f32vec3(0.0f, 0.0f, 1.0f),
-//		glv::f32vec3(0.0f, 0.0f, 1.0f),
-//		glv::f32vec3(0.0f, 0.0f, 1.0f),
-//		glv::f32vec3(0.0f, 0.0f, 1.0f),
-//		glv::f32vec3(0.0f, 0.0f, 1.0f),
-//
-//		glv::f32vec3(-1.0f, 0.0f, 0.0f),
-//		glv::f32vec3(-1.0f, 0.0f, 0.0f),
-//		glv::f32vec3(-1.0f, 0.0f, 0.0f),
-//		glv::f32vec3(-1.0f, 0.0f, 0.0f),
-//		glv::f32vec3(-1.0f, 0.0f, 0.0f),
-//		glv::f32vec3(-1.0f, 0.0f, 0.0f),
-//
-//		glv::f32vec3(1.0f, 0.0f, 0.0f),
-//		glv::f32vec3(1.0f, 0.0f, 0.0f),
-//		glv::f32vec3(1.0f, 0.0f, 0.0f),
-//		glv::f32vec3(1.0f, 0.0f, 0.0f),
-//		glv::f32vec3(1.0f, 0.0f, 0.0f),
-//		glv::f32vec3(1.0f, 0.0f, 0.0f),
-//
-//		glv::f32vec3(0.0f, -1.0f, 0.0f),
-//		glv::f32vec3(0.0f, -1.0f, 0.0f),
-//		glv::f32vec3(0.0f, -1.0f, 0.0f),
-//		glv::f32vec3(0.0f, -1.0f, 0.0f),
-//		glv::f32vec3(0.0f, -1.0f, 0.0f),
-//		glv::f32vec3(0.0f, -1.0f, 0.0f),
-//
-//		glv::f32vec3(0.0f, 1.0f, 0.0f),
-//		glv::f32vec3(0.0f, 1.0f, 0.0f),
-//		glv::f32vec3(0.0f, 1.0f, 0.0f),
-//		glv::f32vec3(0.0f, 1.0f, 0.0f),
-//		glv::f32vec3(0.0f, 1.0f, 0.0f),
-//		glv::f32vec3(0.0f, 1.0f, 0.0f),
-//};
-//
-//std::vector<glv::f32vec2> uvs{
-//		glv::f32vec2(0.0f, 0.0f),
-//		glv::f32vec2(1.0f,  0.0f),
-//		glv::f32vec2(1.0f,  1.0f),
-//		glv::f32vec2(1.0f,  1.0f),
-//		glv::f32vec2(0.0f,  1.0f),
-//		glv::f32vec2(0.0f,  0.0f),
-//
-//		glv::f32vec2(0.0f,  0.0f),
-//		glv::f32vec2(1.0f,  0.0f),
-//		glv::f32vec2(1.0f,  1.0f),
-//		glv::f32vec2(1.0f,  1.0f),
-//		glv::f32vec2(0.0f,  1.0f),
-//		glv::f32vec2(0.0f,  0.0f),
-//
-//		glv::f32vec2(1.0f,  0.0f),
-//		glv::f32vec2(1.0f,  1.0f),
-//		glv::f32vec2(0.0f,  1.0f),
-//		glv::f32vec2(0.0f,  1.0f),
-//		glv::f32vec2(0.0f,  0.0f),
-//		glv::f32vec2(1.0f,  0.0f),
-//
-//		glv::f32vec2(1.0f,  0.0f),
-//		glv::f32vec2(1.0f,  1.0f),
-//		glv::f32vec2(0.0f,  1.0f),
-//		glv::f32vec2(0.0f,  1.0f),
-//		glv::f32vec2(0.0f,  0.0f),
-//		glv::f32vec2(1.0f,  0.0f),
-//
-//		glv::f32vec2(0.0f,  1.0f),
-//		glv::f32vec2(1.0f,  1.0f),
-//		glv::f32vec2(1.0f,  0.0f),
-//		glv::f32vec2(1.0f,  0.0f),
-//		glv::f32vec2(0.0f,  0.0f),
-//		glv::f32vec2(0.0f,  1.0f),
-//
-//		glv::f32vec2(0.0f,  1.0f),
-//		glv::f32vec2(1.0f,  1.0f),
-//		glv::f32vec2(1.0f,  0.0f),
-//		glv::f32vec2(1.0f,  0.0f),
-//		glv::f32vec2(0.0f,  0.0f),
-//		glv::f32vec2(0.0f,  1.0f)
-//};
-//
-//std::vector<glv::f32vec3> positions{
-//		glv::f32vec3(-0.5f, -0.5f, -0.5f),
-//		glv::f32vec3( 0.5f, -0.5f, -0.5f),
-//		glv::f32vec3( 0.5f,  0.5f, -0.5f),
-//		glv::f32vec3( 0.5f,  0.5f, -0.5f),
-//		glv::f32vec3(-0.5f,  0.5f, -0.5f),
-//		glv::f32vec3(-0.5f, -0.5f, -0.5f),
-//
-//		glv::f32vec3(-0.5f, -0.5f,  0.5f),
-//		glv::f32vec3( 0.5f, -0.5f,  0.5f),
-//		glv::f32vec3( 0.5f,  0.5f,  0.5f),
-//		glv::f32vec3( 0.5f,  0.5f,  0.5f),
-//		glv::f32vec3(-0.5f,  0.5f,  0.5f),
-//		glv::f32vec3(-0.5f, -0.5f,  0.5f),
-//
-//		glv::f32vec3(-0.5f,  0.5f,  0.5f),
-//		glv::f32vec3(-0.5f,  0.5f, -0.5f),
-//		glv::f32vec3(-0.5f, -0.5f, -0.5f),
-//		glv::f32vec3(-0.5f, -0.5f, -0.5f),
-//		glv::f32vec3(-0.5f, -0.5f,  0.5f),
-//		glv::f32vec3(-0.5f,  0.5f,  0.5f),
-//
-//		glv::f32vec3( 0.5f,  0.5f,  0.5f),
-//		glv::f32vec3( 0.5f,  0.5f, -0.5f),
-//		glv::f32vec3( 0.5f, -0.5f, -0.5f),
-//		glv::f32vec3( 0.5f, -0.5f, -0.5f),
-//		glv::f32vec3( 0.5f, -0.5f,  0.5f),
-//		glv::f32vec3( 0.5f,  0.5f,  0.5f),
-//
-//		glv::f32vec3(-0.5f, -0.5f, -0.5f),
-//		glv::f32vec3( 0.5f, -0.5f, -0.5f),
-//		glv::f32vec3( 0.5f, -0.5f,  0.5f),
-//		glv::f32vec3( 0.5f, -0.5f,  0.5f),
-//		glv::f32vec3(-0.5f, -0.5f,  0.5f),
-//		glv::f32vec3(-0.5f, -0.5f, -0.5f),
-//
-//		glv::f32vec3(-0.5f,  0.5f, -0.5f),
-//		glv::f32vec3( 0.5f,  0.5f, -0.5f),
-//		glv::f32vec3( 0.5f,  0.5f,  0.5f),
-//		glv::f32vec3( 0.5f,  0.5f,  0.5f),
-//		glv::f32vec3(-0.5f,  0.5f,  0.5f),
-//		glv::f32vec3(-0.5f,  0.5f, -0.5f)
-//};
-
-
-
-void test0();
-
-int main()
-{
-	test0();
-}
-
 enum class gl_debug_type : GLenum
 {
     DEPRECATED_BEHAVIOR = GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR,
@@ -324,7 +122,6 @@ MessageCallback( GLenum source,
 
 }
 
-
 void load_templates(const std::string& include_token)
 {
     static std::string _templates_path = "G:/PrivateRepos/Karamays/_KaramayEngine/karamay_engine_graphics_unit_cmake/karamay_engine_graphics_unit/shaders/templates";
@@ -347,49 +144,30 @@ void load_templates(const std::string& include_token)
         content.size(), content.c_str());
 }
 
-struct wait
+int main()
 {
-    std::string name = "ak";
-    int age = 17;
-};
+    /*  lua_api::lua_vm _lvm;
+  _lvm.start();
+  _lvm.do_file("C:\\PrivateRepos\\Karamays\\_KaramayEngine\\karamay_engine_graphics_unit_cmake\\karamay_engine_graphics_unit\\scripts\\lua\\Test.lua");*/
 
-void to_json(nlohmann::json& j, const wait& w)
-{
-    j = nlohmann::json{ {"name", w.name}, {"age", w.age} };
-}
-
-void from_json(const nlohmann::json& j, wait& w)
-{
-    w.name = j["name"];
-    w.age = j["age"];
-}
-
-
-void test0()
-{
-
-  /*  lua_api::lua_vm _lvm;
-    _lvm.start();
-    _lvm.do_file("C:\\PrivateRepos\\Karamays\\_KaramayEngine\\karamay_engine_graphics_unit_cmake\\karamay_engine_graphics_unit\\scripts\\lua\\Test.lua");*/
-
-	auto* window = new glfw_window();
-	window->load_context();
+    auto* window = new glfw_window();
+    window->load_context();
     std::cout << "VENDOR: " << glGetString(GL_VENDOR) << std::endl;
-	std::cout << "VERSION: " << glGetString(GL_VERSION) << std::endl;
-	std::cout << "RENDERER: " << glGetString(GL_RENDERER) << std::endl;
-	std::cout << "SHADING_LANGUAGE_VERSION: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
-	//std::cout << "GL_EXTENSIONS: " << glGetString(GL_EXTENSIONS) << std::endl;
-	glewInit();
-	glViewport(0, 0, window->get_framebuffer_width(), window->get_framebuffer_height());
+    std::cout << "VERSION: " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "RENDERER: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "SHADING_LANGUAGE_VERSION: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+    //std::cout << "GL_EXTENSIONS: " << glGetString(GL_EXTENSIONS) << std::endl;
+    glewInit();
+    glViewport(0, 0, window->get_framebuffer_width(), window->get_framebuffer_height());
 
 #ifdef _DEBUG
-	glEnable(GL_DEBUG_OUTPUT);
-	glDebugMessageCallback(MessageCallback, 0);
+    glEnable(GL_DEBUG_OUTPUT);
+    glDebugMessageCallback(MessageCallback, 0);
     glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
 #endif 
-   
+
     //auto _file = gltf_loader::load("");
-    
+
     load_templates("/common.glsl");
     load_templates("/common.frag.glsl");
 
@@ -410,7 +188,7 @@ void test0()
     uint32 _frame_count = 0;
     float _frame_delta_time = 0.0f;
     while (_standby_renderers.size())
-	{
+    {
         _frame_delta_time = 0.0f;
         auto _frame_start = std::chrono::steady_clock::now();
         window->tick(0.0f);
@@ -420,229 +198,5 @@ void test0()
         ++_frame_count;
 
         if (_frame_count == 500) break;
-	}
-
+    }
 }
-
-//
-//void test1()
-//{
-//	glfw_window* window = new glfw_window();
-//	window->load_context();
-//
-//	glewInit();
-//
-//
-//	/*auto ubod = std::make_shared<gl_uniform_buffer_descriptor>();
-//
-//	ubod->add_uniform(glm::vec4(0.0f, 1.0f, 0.5f, 0.0f));
-//	ubod->add_uniform(glm::vec3(1.2f, 1.6f, 0.7f));
-//	ubod->add_uniform(glm::mat4(0.0f));*/
-//
-//
-//	//std::uint8_t* data = (std::uint8_t*)ubod->get_data();
-//	//
-//	//glm::vec3* d = (glm::vec3*)(data + 16);
-//
-//	/*const void* _data = vao->get_mapped_data();
-//	const glv_vec3* _dv = (glv_vec3*)_data;
-//	for (int i = 0; i < 36; ++i)
-//	{
-//		std::cout << _dv[i].r << "," << _dv[i].g << "," << _dv[i].b << std::endl;
-//	}*/
-//
-//
-//	//std::cout << "size :" << vao->get_attribute_components_num(0) << std::endl;
-//	//std::cout << "size :" << vao->get_attribute_components_num(1) << std::endl;
-//	//std::cout << "size :" << vao->get_attribute_components_num(2) << std::endl;
-//
-//
-//	// camera / view_matrix/ camera_position
-//	auto camera = std::make_shared<gl_camera>();
-//
-//	// model_matrix
-//	glm::vec3 Position = glm::vec3(0.0f, 0.0f, 0.0f);
-//	float Pitch, Yaw, Roll;
-//	Pitch = Yaw = Roll = 0.0f;
-//	glm::mat4 model_mat(1.0f);
-//	model_mat = glm::translate(model_mat, Position);
-//	model_mat = glm::rotate(model_mat, glm::radians(Pitch), glm::vec3(1.0f, 0.0f, 0.0f));
-//	model_mat = glm::rotate(model_mat, glm::radians(Yaw), glm::vec3(0.0f, 1.0f, 0.0f));
-//	model_mat = glm::rotate(model_mat, glm::radians(Roll), glm::vec3(0.0f, 0.0f, 1.0f));
-//	// projection_matrix
-//	glm::mat4 proj_matrix
-//		= glm::perspective(glm::radians(45.0f),
-//			(float)window->get_framebuffer_width() / (float)window->get_framebuffer_height(),
-//			0.1f, 40.0f);
-//
-//
-//	// uniforms
-//	auto camera_position
-//		= std::make_shared<gl_variable<glv_vec3>>("camera_position", camera->get_position());
-//	auto projection_matrix
-//		= std::make_shared<gl_variable<glv_mat4>>("projection_matrix", proj_matrix);
-//	auto model_matrix
-//		= std::make_shared<gl_variable<glv_mat4>>("model_matrix", model_mat);
-//	auto view_matrix
-//		= std::make_shared<gl_variable<glv_mat4>>("view_matirx", camera->get_view_matrix());
-//
-//	auto ambient_light
-//		= std::make_shared<gl_variable<glv_vec3>>("en_lights.ambient_light", glv_vec3());
-//
-//	auto directional_light
-//		= std::make_shared<gl_variable<glv_vec3>>("en_lights.directional_light[0]", glv_vec3());
-//
-//	/*auto directional_lights_num = std::make_shared< gl_variable<glv_int>>();
-//
-//	auto point_light = std::make_shared< gl_variable<glv_vec3>>();
-//	auto point_lights_num = std::make_shared< gl_variable<glv_int>>();
-//
-//	auto spot_light = std::make_shared< gl_variable<glv_vec3>>();
-//	auto spot_lights_num = std::make_shared< gl_variable<glv_int>>();*/
-//
-//	// textures
-//	auto container2 = std::make_shared<gl_texture_2d>();
-//	auto albedo_map = std::make_shared<gl_texture_2d>();
-//	auto normal_map = std::make_shared<gl_texture_2d>();
-//	//auto metalness_map = std::make_shared<gl_texture_2d>();
-//	auto roughness_map = std::make_shared<gl_texture_2d>();
-//	auto displacement_map = std::make_shared<gl_texture_2d>();
-//	auto ambient_occlusion_map = std::make_shared<gl_texture_2d>();
-//
-//	/*gl_texture_pixels container2_pixels("assets/Textures/container2.png");
-//	container2->fill(container2_pixels.width, container2_pixels.height, container2_pixels.format, (const void*)container2_pixels.pixels);
-//	container2->set_name("container2");*/
-//
-//	gl_texture_pixels albedo_pixels("assets/Materials/Rock030_2k-JPG/Rock030_2K_Color.jpg");
-//	albedo_map->fill(albedo_pixels.width, albedo_pixels.height, albedo_pixels.format, (const void*)albedo_pixels.pixels);
-//	albedo_map->set_name("mat.albedo_map");
-//
-//	gl_texture_pixels normal_pixels("assets/Materials/Rock030_2k-JPG/Rock030_2K_Normal.jpg");
-//	normal_map->fill(normal_pixels.width, normal_pixels.height, normal_pixels.format, (const void*)normal_pixels.pixels);
-//	normal_map->set_name("mat.normal_map");
-//
-//	/*gl_texture_pixels metalness_pixels("assets/Materials/Rock030_2k-JPG/Rock030_2K_.jpg");
-//	metalness_map->fill(metalness_pixels.width, metalness_pixels.height, (const void*)metalness_pixels.pixels);
-//	metalness_map->set_name("mat.metalness_map");*/
-//
-//	gl_texture_pixels roughness_pixels("assets/Materials/Rock030_2k-JPG/Rock030_2K_Roughness.jpg");
-//	roughness_map->fill(roughness_pixels.width, roughness_pixels.height, roughness_pixels.format, (const void*)roughness_pixels.pixels);
-//	roughness_map->set_name("mat.roughness_map");
-//
-//	gl_texture_pixels displacement_pixels("assets/Materials/Rock030_2k-JPG/Rock030_2K_Displacement.jpg");
-//	displacement_map->fill(displacement_pixels.width, displacement_pixels.height, displacement_pixels.format, (const void*)displacement_pixels.pixels);
-//	displacement_map->set_name("mat.displacement_map");
-//
-//	gl_texture_pixels ambient_occlusion_pixels("assets/Materials/Rock030_2k-JPG/Rock030_2K_AmbientOcclusion.jpg");
-//	ambient_occlusion_map->fill(ambient_occlusion_pixels.width, ambient_occlusion_pixels.height, ambient_occlusion_pixels.format, (const void*)ambient_occlusion_pixels.pixels);
-//	ambient_occlusion_map->set_name("mat.ambient_occlusion_map");
-//
-//
-//	glViewport(0, 0, window->get_framebuffer_width(), window->get_framebuffer_height());
-//
-//	/*std::vector<glv_vec3> poss{
-//		glv_vec3(0.5f, 0.5f, 0.0f),
-//		glv_vec3(0.5f, -0.5f, 0.0f),
-//		glv_vec3(-0.5f, -0.5f, 0.0f),
-//		glv_vec3(-0.5f, 0.5f, 0.0f)
-//	};
-//
-//	std::vector<glv_vec2> uvss{
-//		glv_vec2(1.0f, 1.0f),
-//		glv_vec2(1.0f, 0.0f),
-//		glv_vec2(0.0f, 0.0f),
-//		glv_vec2(0.0f, 1.0f)
-//	};
-//
-//	std::vector<glm::uint32> indices{
-//		0, 1, 3,
-//		1, 2, 3
-//	};*/
-//
-//	std::size_t size = positions.size();
-//
-//	// vertex array
-//	auto vaod = sptr(gl_vertex_array_descriptor);
-//	//vaod->add_attributes<glv_vec3>(poss);
-//	//vaod->add_attributes<glv_vec2>(uvss);
-//	vaod->add_attributes<glv_vec3>(positions);
-//	vaod->add_attributes<glv_vec2>(uvs);
-//	vaod->add_attributes<glv_vec3>(normals);
-//	auto vao = sp_vertex_array(vaod);
-//
-//	// element array
-//	/*auto ebod = std::make_shared<gl_element_array_buffer_descriptor>();
-//	ebod->set_indices(indices);
-//	auto ebo = std::make_shared<gl_element_array_buffer>();
-//	ebo->fill(ebod);*/
-//
-//	// program
-//	auto program = std::make_shared<gl_program>();
-//	/*program->construct({
-//		"shaders/test.vert",
-//		"shaders/test.frag" });*/
-//	program->construct({
-//		"shaders/Mesh/PBRMesh/PBRMesh.vert",
-//		"shaders/Mesh/PBRMesh/PBRMesh.tesc",
-//		"shaders/Mesh/PBRMesh/PBRMesh.tese",
-//		"shaders/Mesh/PBRMesh/PBRMesh.geom",
-//		"shaders/Mesh/PBRMesh/PBRMesh.frag" });
-//	// ...
-//	program->set_vertex_array(vao);
-//	// ...
-//	program->add_texture(albedo_map);
-//	program->add_texture(normal_map);
-//	program->add_texture(roughness_map);
-//	program->add_texture(displacement_map);
-//	program->add_texture(ambient_occlusion_map);
-//	// ...
-//	program->add_uniform(camera_position);
-//	program->add_uniform(projection_matrix);
-//	program->add_uniform(view_matrix);
-//	program->add_uniform(model_matrix);
-//	// ...
-//	program->set_framebuffer();
-//	program->set_commands([&size] {
-//		//glDrawArrays(GL_TRIANGLES, 0, 5);
-//		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-//
-//		glPatchParameteri(GL_PATCH_VERTICES, 3);
-//		glDrawArraysInstancedBaseInstance(GL_PATCHES, 0, size, 1, 0);
-//		});
-//
-//
-//	//glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); //GL_FILL GL_POINT
-//	//glFrontFace(GL_CW);//default ��ʱ�� GL_CW ˳ʱ��
-//
-//	//glEnable(GL_COLOR_BUFFER_BIT);
-//	//glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
-//	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-//
-//	//glEnable(GL_ACCUM_BUFFER_BIT);
-//	//glClearAccum(1.0f, 0.2f, 1.0f, 1.0f);
-//
-//	// when current frag-z < 1.0, keep it
-//	// fragShader write its z value before working
-//	//glEnable(GL_DEPTH_TEST);
-//	//glDepthFunc(GL_LESS);
-//	//glDepthMask(GL_TRUE);
-//	//glClearDepth(1.0f);
-//
-//	//glEnable(GL_STENCIL_TEST);
-//	//glStencilFunc(GL_ALWAYS, 1, 0xFF);
-//	//glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
-//	//glStencilMask(GL_TRUE);
-//	//glClearStencil(0);
-//
-//
-//
-//	int i = 1;
-//	while (i--)
-//	{
-//
-//		//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-//		glClear(GL_COLOR_BUFFER_BIT);
-//		program->render(0.0f);
-//		window->tick(0.0f);
-//	}
-//}
